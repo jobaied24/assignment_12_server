6a1558d7b168e42916d5bca4b1a3aca056d3e7c8
hero00 <jobaied543@gmail.com>
2026-06-16 19:09:01 +0600

    initialize mongodb setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
 const app=express();
@@ -15,6 +16,38 @@ app.use(express.json());
 app.get('/',(req,res)=>{
     res.send('Campcure server is running');
 });
+
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hota77b.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db=client.db('campcure_db');
+    const campsCollection = db.collection('camps')
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.listen(port,()=>{
